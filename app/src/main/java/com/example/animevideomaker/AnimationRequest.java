f0b039dd2218d3c4c9dad1f869de4be0d0c1b890
Mr-H9542 <hamza954233@gmail.com>
2025-07-01 22:33:42 +0400

    Create AnimationRequest.java

---
+++ app/src/main/java/com/example/animevideomaker/AnimationRequest.java
@@ -0,0 +1,9 @@
+package com.example.animevideomaker;
+
+public class AnimationRequest {
+    public String characterType = "star";
+    public String characterColor = "blue";
+    public String action = "bounce";
+    public String background = "black";
+    public int duration = 5;
+}
