93a447591aa417e801971655bf60eb33ee490185
Mr-H9542 <hamza954233@gmail.com>
2025-07-01 19:07:11 +0400

    Update and rename NotificationManager.java to AppNotifier.java

--- app/src/main/java/com/example/animevideomaker/AppNotifier.java
+++ app/src/main/java/com/example/animevideomaker/AppNotifier.java
@@ -3,7 +3,7 @@ package com.example.animevideomaker;
 import android.content.Context;
 import android.widget.Toast;
 
-public class NotificationManager {
+public class AppNotifier {
 
     public static void showWarning(Context context, String message) {
         Toast.makeText(context, "⚠️ " + message, Toast.LENGTH_SHORT).show();
