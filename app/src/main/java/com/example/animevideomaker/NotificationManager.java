ae10ce6a243b9c9d85f4a7f16a106ba82b1ec5f2
Mr-H9542 <hamza954233@gmail.com>
2025-07-01 19:00:09 +0400

    Create NotificationManager.java

---
+++ app/src/main/java/com/example/animevideomaker/NotificationManager.java
@@ -0,0 +1,15 @@
+package com.example.animevideomaker;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NotificationManager {
+
+    public static void showWarning(Context context, String message) {
+        Toast.makeText(context, "⚠️ " + message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showCritical(Context context, String message) {
+        Toast.makeText(context, "🔥 " + message, Toast.LENGTH_LONG).show();
+    }
+}
