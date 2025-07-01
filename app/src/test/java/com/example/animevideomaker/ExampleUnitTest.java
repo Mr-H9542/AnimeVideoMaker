ba41edef3614c8e3b389bf69ec2dbc27640c4835
Mr-H9542 <hamza954233@gmail.com>
2025-07-01 21:41:29 +0400

    Create ExampleUnitTest.java

---
+++ app/src/test/java/com/example/animevideomaker/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.animevideomaker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2); // ✅ This test should pass
+    }
+}
