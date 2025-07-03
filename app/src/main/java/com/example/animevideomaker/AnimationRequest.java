5bce707db6053ea9e04330cbd5c3ae1d63581755
Mr-H9542 <hamza954233@gmail.com>
2025-07-03 20:41:21 +0400

    Update AnimationRequest.java

--- app/src/main/java/com/example/animevideomaker/AnimationRequest.java
+++ app/src/main/java/com/example/animevideomaker/AnimationRequest.java
@@ -1,11 +1,24 @@
 package com.example.animevideomaker;
 
 public class AnimationRequest {
-    public String characterType = "star";
-    public String characterColor = "blue";
-    public String action = "bounce";
-    public String background = "black";
-    public int duration = 5;
+    public String characterType;
+    public String characterColor;
+    public String action;
+    public String background;
+    public int duration;
+
+    public AnimationRequest() {
+        this("star", "blue", "bounce", "black", 5);
+    }
+
+    public AnimationRequest(String characterType, String characterColor,
+                            String action, String background, int duration) {
+        this.characterType = characterType;
+        this.characterColor = characterColor;
+        this.action = action;
+        this.background = background;
+        this.duration = duration;
+    }
 
     @Override
     public String toString() {
